78ce9d2bbcfbf887243ed09f828ac3062094dc3d
Błażej Jaskółowski <blazejjaskolowski@gmail.com>
2025-06-26 17:32:48 +0200

    Pliki projektów z dysku lokalnego.

---
+++ C. Zadanie_11/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Napisz program obliczający sumę n początkowych liczb nieparzystych. Liczbę n należy pobrać od użytkownika. 
+ * Przykładowo dla n=3 należy obliczyć sumę 1+3+5. */
+
+namespace C.Zadanie_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, suma = 0;
+
+            Console.Write("Podaj ilość liczb nieparzystych do zsumowania (n): ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int liczba = 2 * i + 1; // generuje kolejne liczby nieparzyste: 1, 3, 5, 7...
+                suma += liczba;
+
+                // wyświetlenie procesu
+                Console.Write(liczba);
+                if (i < n - 1)
+                    Console.Write("+");
+            }
+
+            Console.WriteLine($"={suma}");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ C. Zadanie_5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/* Wczytaj liczbę naturalną n (n>1). Narysuj na ekranie z gwiazdek trójkąt prostokątny. 
+ * Użyj pętli for. Przykładowo dla n=3 narysowany trójkąt powinien wyglądać:
+    *
+    **
+    ***    */
+
+
+
+namespace C.Zadanie_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, n;
+            Console.Write("Podaj liczbę naturalną n>1: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 1; i <= n; i++) //liczba wierszy
+            {
+                for (j = 1; j <= i; j++) //liczba kolumn
+                    Console.Write("*");
+                Console.WriteLine(""); //nowa linia
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
